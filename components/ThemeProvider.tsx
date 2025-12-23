d9479a9405488c920d0939605090ec952a0a3e61
DaniIliev <danieljivkov04@abv.bg>
2025-12-23 11:35:06 +0200

    fix the hidden space

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -58,7 +58,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme: handleSetTheme }}>
-            {children}   {" "}
+      {children}
     </ThemeContext.Provider>
   );
 }
